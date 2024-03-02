8101aefd50a57b197503d2f84e6a23beefd3a6b6
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-03-02 14:59:39 +0530

    update

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import ClientOnly from "./components/ClientOnly";
+import LoginModal from "./components/modals/LoginModal";
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {/* <ClientOnly> */}
+        <ToasterProvider />
+        <LoginModal />
+        <RegisterModal />
+        <Navbar />
+        {/* </ClientOnly> */}
+        {children}
+      </body>
+    </html>
+  );
+}
